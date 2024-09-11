9d90b0fbc629cd30c86f60277645b1db64194b1d
yuliiaivanova <gerda88823@gmail.com>
2024-09-11 14:48:06 +0200

    Task1

---
+++ src/main/java/Task1/Books.java
@@ -0,0 +1,7 @@
+package Task1;
+
+public class Books {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/Task1/Songs.java
@@ -0,0 +1,8 @@
+package Task1;
+
+public class Songs {
+    public static void main(String[] args) {
+        System.out.println("Песни");
+    }
+
+}
